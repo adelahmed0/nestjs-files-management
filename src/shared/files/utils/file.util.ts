2029108c7d332a421d56259b9a418490043e559d
Adel Ahmed <adel15952@gmail.com>
2026-04-25 19:00:42 +0300

    Enhance file utility with mime-types integration
    - Update createFileTypeRegex to use mime-types library for accurate MIME type detection
    - Add proper MIME type lookup and filtering for better file type validation
    - Update yarn.lock with new dependency

--- src/shared/files/utils/file.util.ts
+++ src/shared/files/utils/file.util.ts
@@ -1,4 +1,10 @@
 import { FileType } from '../types/file.types';
+import { lookup } from 'mime-types';
 
-export const createFileTypeRegex = (fileTypes: FileType[]): RegExp =>
-  new RegExp(fileTypes.join('|'), 'i');
+export const createFileTypeRegex = (fileTypes: FileType[]): RegExp => {
+  // TODO: Use mime-types to get the correct regex
+  const mimeTypes = fileTypes
+    .map((type) => lookup(type))
+    .filter((type) => type !== false);
+  return new RegExp(mimeTypes.join('|'), 'i');
+};
